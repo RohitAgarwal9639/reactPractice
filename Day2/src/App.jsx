3c1c8ddd452e1e9836faf1eefec89f7f5d84c78d
Rohit <ro8267856984@gmail.com>
2025-06-25 14:44:43 +0530

    Some more component practice

---
+++ Day2/src/App.jsx
@@ -0,0 +1,16 @@
+import Footer from "./Components/Footer"
+import Header from "./Components/header"
+import Main from "./Components/main"
+
+const App=()=>{
+  return (
+    <div>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </div>
+    
+  )
+}
+
+export default App
