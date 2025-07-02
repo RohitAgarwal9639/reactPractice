d98ee305f26af18870818f936a134591cfc6442f
Rohit <ro8267856984@gmail.com>
2025-07-02 15:04:44 +0530

    Portal concept done

---
+++ Day9/src/App.jsx
@@ -0,0 +1,15 @@
+import CopyInput from "./Components/CopyInput"
+import Switcher from "./Components/Switcher"
+
+const App=()=>{
+  
+  return (
+    <main>
+      <CopyInput />
+      <Switcher />
+    </main>
+    
+  )
+}
+
+export default App
