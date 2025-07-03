3a88e8b10a2ed111276c033ed57f645687eb665c
Rohit <ro8267856984@gmail.com>
2025-07-03 15:26:53 +0530

    UseContext practice

---
+++ Day14/src/App.jsx
@@ -0,0 +1,16 @@
+import UpdateUser from "./Components/UpdateUser"
+import UserProfile from "./Components/UserProfile"
+import { UserProvider } from "./UserContext"
+
+const App=()=>{
+  
+  return (
+    <UserProvider>
+      <UserProfile />
+      <UpdateUser />
+    </UserProvider>
+    
+  )
+}
+
+export default App
---
+++ Day14/src/UserContext.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { createContext,useState } from 'react'
+const UserContext = createContext();
+const UserProvider = ({ children }) => {
+    const [user,setUser] = useState({name: "John Doe"});
+    const updateUser = (newUser) => {
+        setUser({name: newUser });
+    }
+    return (<UserContext.Provider value={{user,updateUser}}>{children}</UserContext.Provider>)
+}
+export { UserContext, UserProvider };
