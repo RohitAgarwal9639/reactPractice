27440173f69d1ce617a58705545139992e8a9b8d
Rohit <ro8267856984@gmail.com>
2025-07-04 11:47:56 +0530

    UseId Hook

---
+++ Day19/src/App.jsx
@@ -0,0 +1,15 @@
+import UniqueId from "./Components/UniqueId"
+
+const App=()=>{
+  
+  return (
+    <main>
+      <UniqueId />
+      <p> lrembsdkfmsdfnk </p>
+      <UniqueId />
+    </main>
+    
+  )
+}
+
+export default App
---
+++ Day19/src/Components/UniqueId.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useId } from 'react'
+const UniqueId = () => {
+    const id = useId();
+  return (
+    <div>
+      <label htmlFor={`${id}-email`}>Email:</label>
+      <input type="email" id={`${id}-email`} />
+
+      <br />
+      <label htmlFor={`${id}-password`}>Password:</label>
+      <input type="password" id={`${id}-password`} />
+    </div>
+  )
+}
+
+export default UniqueId
